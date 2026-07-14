d7f5423e96a88325b44de690057ecd514ade32f6
Daru <ozziewei@gmail.com>
2026-07-14 16:07:10 +0800

    [NFC]Fix include guard

--- include/ucrash/DllExport/DllExport.h
+++ include/ucrash/DllExport/DllExport.h
@@ -1,14 +1,20 @@
-#ifndef DLLEXPORT_H
-#define DLLEXPORT_H
+#ifndef UCRASH_DLLEXPORT_H
+#define UCRASH_DLLEXPORT_H
 
 #ifdef _WIN32
+#ifdef DllExport
+#undef DllExport
+#endif
 #if UCRASH_DLL
 #define DllExport __declspec(dllexport)
 #else
 #define DllExport __declspec(dllimport)
 #endif // UCRASH_DLL
 #else
+#ifdef DllExport
+#undef DllExport
+#endif
 #define DllExport
 #endif // _WIN32
 
-#endif // DLLEXPORT_H
+#endif // UCRASH_DLLEXPORT_H
